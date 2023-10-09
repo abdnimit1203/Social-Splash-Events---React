79e57cc6b06440df7fcaac62633b77a44eca2e5e
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2023-10-09 12:46:45 +0600

    More bug fixes and navigation added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Services = () => {
   return (
@@ -12,8 +13,8 @@ const Services = () => {
           <h2 className="font-semibold text-xl">Day of Coordination</h2>
           <ul className="py-8 space-y-2">
             <li className="flex text-slate-500 text-sm">
-              <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                2 hours of professional in-person consultation  
+              <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />2
+              hours of professional in-person consultation
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
@@ -29,88 +30,87 @@ const Services = () => {
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
-                
-                Development of a detailed wedding day timeline and layout
-                
+              Development of a detailed wedding day timeline and layout
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
-                Assistance with wedding set-up and tear down  
+              Assistance with wedding set-up and tear down
             </li>
           </ul>
-          <button className="btn border-secondary rounded-sm hover:btn-secondary text-secondary bg-white">Purchase</button>
+          <Link to={"/"}>
+            <button className="btn border-secondary rounded-sm hover:btn-secondary text-secondary bg-white">
+              Purchase
+            </button>
+          </Link>
         </div>
         <div className="max-w-xs   ">
           <h2 className="font-semibold text-xl">Full Service Planning</h2>
           <ul className="py-8 space-y-2 list-disc">
             <li className="flex text-slate-500 text-sm">
               <AiFillHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
-                
-                Unlimited consultations with client via telephone/email (when
-                available)
-                
+              Unlimited consultations with client via telephone/email (when
+              available)
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiFillHeart className="my-1  mr-4 text-xl text-rose-600" />
-                
-                Full wedding planning timeline formation and management
-                
+              Full wedding planning timeline formation and management
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiFillHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
-                Budget formation and management  
+              Budget formation and management
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiFillHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
-                Assistance with wedding design  
+              Assistance with wedding design
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiFillHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
-                Vendor referrals  
+              Vendor referrals
             </li>
             <li className="flex text-slate-500 text-sm">
-              <AiFillHeart className="my-1  mr-4 text-xl text-rose-600" />
-                
-                {" "}
-                Attendance at all vendor meetings (schedule permitting)
-                
+              <AiFillHeart className="my-1  mr-4 text-xl text-rose-600" />{" "}
+              Attendance at all vendor meetings (schedule permitting)
             </li>
           </ul>
-          <button className="btn  rounded-sm btn-secondary">Purchase</button>
+          <Link to={"/"}>
+            <button className="btn  rounded-sm btn-secondary">Purchase</button>
+          </Link>
         </div>
         <div className="max-w-x ">
           <h2 className="font-semibold text-xl">Exclusive Deal</h2>
           <ul className="py-8 space-y-2">
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                 Reviewing wedding budget  
+              Reviewing wedding budget
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                 Reviewing wedding planning timeline  
+              Reviewing wedding planning timeline
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                 Pulling together design ideas  
+              Pulling together design ideas
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                 Suggesting vendors  
+              Suggesting vendors
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                 Reviewing venue options  
+              Reviewing venue options
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                 Centerpiece design and assembly  
+              Centerpiece design and assembly
             </li>
             <li className="flex text-slate-500 text-sm">
               <AiOutlineHeart className="my-1  mr-4 text-xl text-rose-600" />
-                 Wedding favor design and assembly  
+              Wedding favor design and assembly
             </li>
           </ul>
-          <button className="btn  rounded-sm btn-secondary">Purchase</button>
+          <Link to={"/"}>
+            <button className="btn  rounded-sm btn-secondary">Purchase</button>
+          </Link>
         </div>
       </div>
     </>
